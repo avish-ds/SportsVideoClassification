783a3157625a0c6adf5aff19b232cdac0a11a97e
unknown <shreyampadmashali15@gmail.com>
2023-04-05 12:53:53 +0530

    resolved error for empty/none frame

--- TestOutput.py
+++ TestOutput.py
@@ -8,9 +8,9 @@ import cv2
 
 #loading the model first
 def process_video(path):
-    model=load_model(r"D:\Sports Video Classification\SportsVideoClassification\videoClassificationModel")
-    lb=pickle.loads(open(r"D:\Sports Video Classification\SportsVideoClassification\videoClassificationBinarizer.pickle","rb").read())
-    outputVideo1=r"D:\Sports Video Classification\SportsVideoClassification\outputVideos\output03.avi"
+    model=load_model(r"C:\SportsVideoClassification\videoClassificationModel")
+    lb=pickle.loads(open(r"C:\SportsVideoClassification\videoClassificationBinarizer.pickle","rb").read())
+    outputVideo1=r"C:\SportsVideoClassification\outputVideos\output04.avi"
     mean=np.array([123.68,116.779,103.939][::1],dtype="float32")
     Queue=deque(maxlen=128)
     capture_video=cv2.VideoCapture(path)
@@ -21,7 +21,9 @@ def process_video(path):
         (taken,frame)=capture_video.read()
         if not taken:
             break
-        if frame_no%1000==0:
+        if frame is None:
+            continue
+        if frame_no%10==0:
             if Width is None or Height is None:
                 (Width,Height)=frame.shape[:2]
             output=frame.copy()
@@ -38,12 +40,11 @@ def process_video(path):
             # obj=SportsVideoClassification()
             # obj.item="Sport is Table Tennis"
             # print(text)
-            # cv2.putText(output,text,(45,60),cv2.FONT_HERSHEY_COMPLEX,1.25,(255,0,0),5)
+            cv2.putText(output,text,(45,60),cv2.FONT_HERSHEY_COMPLEX,1.25,(255,0,0),5)
     
             if writer is None:
                 fourcc=cv2.VideoWriter_fourcc(*"MJPG")
-
-                writer=cv2.VideoWriter("outputVideo",fourcc,30,(Width,Height),True)
+                writer=cv2.VideoWriter("outputVideo1",fourcc,30,(Width,Height),True)
             writer.write(output)
             cv2.imshow("Working",output)
             key=cv2.waitKey(1)&0xFF
@@ -53,4 +54,4 @@ def process_video(path):
     print("Finalizing")
     writer.release()
     capture_video.release()
-process_video(r"D:\Sports Video Classification\SportsVideoClassification\trainingVideos\video1.mp4")
+process_video(r"C:\SportsVideoClassification\trainingVideos\video2q.mp4")
